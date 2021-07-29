f262397b38ec5589e9551db01a0d3398e7753407
ishaan191220 <loombaishaan@gmail.com>
2021-07-29 10:58:33 +0530

    first commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+
+contract TodoList {
+
+    uint public taskCount = 0;
+
+    struct Task{
+
+        uint id;
+        string content;
+        bool completed;
+
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+
+          createTask("Hello To Blockchain 101");
+    }
+
+    function createTask(string memory _content) public{
+
+taskCount ++;
+tasks[taskCount]= Task(taskCount,_content,false);
+
+    }
+    
+    }
